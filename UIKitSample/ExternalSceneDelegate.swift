2a513fe5cb2ac191d876cc7f806d54da77ccbede
cranoo <75974473+cranoo3@users.noreply.github.com>
2026-03-23 02:45:36 +0900

    外部ディスプレイ接続時にUIViewControllerを設定するコードを追加
    info.plistで指定したdelegate class nameを使用し作成する

---
+++ UIKitSample/ExternalSceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  ExternalSceneDelegate.swift
+//  UIKitSample
+//
+//  Created by cranoo on 2026/03/23.
+//
+
+import UIKit
+
+class ExternalSceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene, session.role == .windowExternalDisplayNonInteractive else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = UIViewController()
+        window.isHidden = false
+        self.window = window
+    }
+}
